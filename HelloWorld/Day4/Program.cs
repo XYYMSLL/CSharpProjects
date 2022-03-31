5107567802eeb85b4e2d25b087b7f4bfa50e2d23
XZhou <xyymsll@qq.com>
2022-03-31 14:06:05 +0800

    123

---
+++ HelloWorld/Day4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day4
+{
+
+    struct MyStruct
+    {
+        public MyStruct(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+
+        public override string ToString()
+        {
+            return Name + ", " + Age;
+        }
+
+        public bool Equals(MyStruct other)
+        {
+            return other.Name == Name;
+        }
+    }
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.Write(typeof(string).Assembly.ImageRuntimeVersion);
+        }
+    }
+}
