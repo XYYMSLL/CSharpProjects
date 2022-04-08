80e355a2be99ad80498ab783b6f84435134f4b8d
XZhou <xyymsll@qq.com>
2022-04-08 10:46:27 +0800

    Day9

---
+++ HelloWorld/Day8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Test1
+    {
+        int[] arr = new int[3];
+        public int this[int num]
+        {
+            get => arr[num];
+            set => arr[num] = value;
+        }
+    }
+
+    class Test<T, V>
+        where T : class, new()
+        where V : Test1
+    {
+
+    }
+}
