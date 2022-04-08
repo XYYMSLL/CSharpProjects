ea27ad9719384817ac89482e20e3a937de98aae3
XZhou <xyymsll@qq.com>
2022-04-08 17:09:18 +0800

    dictionary

--- HelloWorld/Day9/Program.cs
+++ HelloWorld/Day9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Day9
@@ -100,6 +101,213 @@ namespace Day9
             private void ThrowError(string msg)
             {
                 throw new Exception(msg);
+            }
+        }
+
+        class MyKvp<TKey, TValue>
+        {
+            TKey _key;
+            TValue _value;
+
+            public TKey Key
+            {
+                get => _key;
+            }
+
+            public TValue Value
+            {
+                get => _value;
+                set => _value = value;
+            }
+
+            public MyKvp(TKey key, TValue value)
+            {
+                _key = key;
+                _value = value;
+            }
+        }
+
+        class MyDictionary<TKey, TValue>
+        {
+            MyKvp<TKey, TValue>[] _data;
+            int count;
+            bool _isReadOnly;
+
+            public ICollection<TKey> Keys
+            {
+                get
+                {
+                    TKey[] keys = new TKey[count];
+                    for (int i = 0; i < count; i++)
+                    {
+                        keys[i] = _data[i].Key;
+                    }
+                    return keys;
+                }
+            }
+
+            public ICollection<TValue> Values
+            {
+                get
+                {
+                    TValue[] values = new TValue[count];
+                    for (int i = 0; i < count; i++)
+                    {
+                        values[i] = _data[i].Value;
+                    }
+                    return values;
+                }
+            }
+
+            public int Count => count;
+
+            public bool IsReadOnly => _isReadOnly;
+
+            public TValue this[TKey key]
+            {
+                get => GetValueByKey(key);
+                set
+                {
+                    if (!_isReadOnly)
+                    {
+                        int i = GetIndexByKey(key);
+
+                        if (i == -1)
+                        { _data[GetIndexByKey(key)].Value = value; }
+                        else
+                        {
+                            Add(key, value);
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Dictionary is read only");
+                    }
+                }
+            }
+
+            public MyDictionary()
+            {
+                _data = new MyKvp<TKey, TValue>[4];
+                count = 0;
+                _isReadOnly = false;
+            }
+
+            public MyDictionary(bool isReadOnly)
+            {
+                _data = new MyKvp<TKey, TValue>[4];
+                count = 0;
+                _isReadOnly = isReadOnly;
+            }
+
+            public TValue GetValueByKey(TKey key)
+            {
+                int i = GetIndexByKey(key);
+
+                if (i >= 0)
+                {
+                    return _data[i].Value;
+                }
+
+                throw new Exception("{key} not exists");
+            }
+
+            public void Add(TKey key, TValue value)
+            {
+                int i = GetIndexByKey(key);
+
+                if (i < 0)
+                {
+                    MyKvp<TKey, TValue> newKvp = new MyKvp<TKey, TValue>(key, value);
+                    if (count >= _data.Length)
+                    {
+                        Expand();
+                    }
+                    _data[count] = newKvp;
+                    count++;
+                    return;
+                }
+
+                throw new Exception("{key} already exists");
+            }
+
+            public bool Remove(TKey key)
+            {
+                int i = GetIndexByKey(key);
+
+                if (i >= 0)
+                {
+                    Array.Copy(_data, i + 1, _data, i, count - i - 1);
+                    count--;
+                    return true;
+                }
+
+                return false;
+            }
+
+            private void Expand()
+            {
+                MyKvp<TKey, TValue>[] newData = new MyKvp<TKey, TValue>[_data.Length * 2];
+                Array.Copy(_data, newData, _data.Length);
+                _data = newData;
+            }
+
+            private int GetIndexByKey(TKey key)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (_data[i].Key.Equals(key))
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+
+            public bool ContainsKey(TKey key)
+            {
+                int i = GetIndexByKey(key);
+                return i != -1;
+            }
+
+            public bool TryGetValue(TKey key, out TValue value)
+            {
+                int i = GetIndexByKey(key);
+
+                if (i >= 0)
+                {
+                    value = _data[i].Value;
+                    return true;
+                }
+
+                value = default(TValue);
+
+                return false;
+            }
+
+            public void Add(MyKvp<TKey, TValue> item)
+            {
+                int i = GetIndexByKey(item.Key);
+
+                if (i < 0)
+                {
+                    if (count >= _data.Length)
+                    {
+                        Expand();
+                    }
+                    _data[count] = item;
+                    count++;
+                    return;
+                }
+
+                throw new Exception("Key name \"{key}\" already exists");
+            }
+
+            public void Clear()
+            {
+                _data = new MyKvp<TKey, TValue>[4];
+                count = 0;
             }
         }
     }
